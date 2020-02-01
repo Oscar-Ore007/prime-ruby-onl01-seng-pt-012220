54436d56e4a8d9599a5ec724ea41e3d55ea7cd35
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 18:31:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+#Fork and clone this repository. Run the test suite to learn what is required of you.
+
+#You'll be defining a method, prime?(), that takes in an integer argument and returns a boolean of whether or not that integer is a prime number.
+
+#A few things to think about:
+
+#What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
+How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
