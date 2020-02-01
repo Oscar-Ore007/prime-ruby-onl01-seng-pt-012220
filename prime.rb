a7e7baedcc952a0c55d5e9280c211626bb15597f
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 18:40:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 
 
-#A few things to think about:
+A few things to think about:
 
-#What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
-#How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
-#(1..10).to_a 
+What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
+How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
+(1..10).to_a 
 
 
 
