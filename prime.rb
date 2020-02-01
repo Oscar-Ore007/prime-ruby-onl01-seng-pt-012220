68759b349466b470bdf6355286db34f520975a99
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 18:54:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+
+# A few things to think about:
+
+# What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
+# How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
+# (1..10).to_a 
+
+
+
+def prime?(number)
+  if number < 2 
+    return false 
+  else 
+    (2..number - 1).to_a.all? do |n| 
+    number % n != 0 
+    end 
+  end 
+end 
+
