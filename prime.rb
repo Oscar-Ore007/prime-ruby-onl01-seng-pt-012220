8b0121f0890023455106b0c3b3b2430c5c8e38cd
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 18:31:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,4 +5,4 @@
 #A few things to think about:
 
 #What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
-How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
+#How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
